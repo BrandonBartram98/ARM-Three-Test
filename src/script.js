f4c76a7288a2c65b418908701e332413f6c27694
Brandon Bartram <brandon@immersivevr.co.uk>
2022-02-25 11:27:05 +0000

    Attempt to fix png

--- src/script.js
+++ src/script.js
@@ -138,7 +138,7 @@ function init() {
     particlesGeometry.setAttribute('rotation', new THREE.BufferAttribute(positions, 3))
 
     const textureLoader = new THREE.TextureLoader()
-    const particleTexture = textureLoader.load('/particles/particles-single.png')
+    const particleTexture = textureLoader.load('/particles/particle.png')
 
     // Material
     const particlesMaterial = new THREE.PointsMaterial({
